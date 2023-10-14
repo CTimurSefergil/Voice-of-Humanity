dfa97779209b0e355d7afe232e7b38f7d3944db2
Timur Sefergil <140344749+CTimurSefergil@users.noreply.github.com>
2023-10-14 19:25:58 +0300

    Update voice_data.rs

--- src/voice_data.rs
+++ src/voice_data.rs
@@ -1,14 +1,9 @@
 use std::fs::File;
+use std::path::Path;
 use std::io::{BufRead, BufReader};
 
-pub(crate) fn read_voice_data(filepath: &str, x: usize) -> Result<String, Box<dyn std::error::Error>> {
-    let file = File::open(filepath)?;
-    let reader = BufReader::new(file);
-    let mut voices = Vec::new();
-
-    for line in reader.lines() {
-        &voices.push(line?);
-    }
-
-    Ok(voices[x].clone())
+pub(crate) fn read_voice_data(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines().map(|l| l.expect("Could not parse line")).collect()
 }
